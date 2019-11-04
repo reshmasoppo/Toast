09721a42681282ac6335f95b653dfa1e69454002
IN007848 <Reshma@oppo2019>
2019-11-04 11:25:38 +0530

    Initial commit

---
+++ app/src/main/java/com/example/toastexample/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.toastexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Gravity
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+
+            Toast.makeText(applicationContext,"this is toast message",Toast.LENGTH_SHORT).show()
+
+            val toast = Toast.makeText(applicationContext, "Hello Javatpoint", Toast.LENGTH_SHORT)
+            toast.show()
+
+            val myToast = Toast.makeText(applicationContext,"toast message with gravity",Toast.LENGTH_SHORT)
+            myToast.setGravity(Gravity.LEFT,200,200)
+            myToast.show()
+
+        }
+    }
+}
